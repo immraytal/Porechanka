74d0e451e3670c28253f82ecba83ef3902f88941
Кисель Андрей Витальевич <AKisel2@alfabank.ru>
2021-05-01 19:25:15 +0300

    Empty project

---
+++ src/main/java/com/kisel/Porechanka/PorechankaApplication.java
@@ -0,0 +1,13 @@
+package com.kisel.Porechanka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PorechankaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PorechankaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kisel/Porechanka/PorechankaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kisel.Porechanka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PorechankaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
